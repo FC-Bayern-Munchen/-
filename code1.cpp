1b607d14f9826927dc3a2988ea5b09f2819ee2c5
wanglu_pro <wanglu_pro@zzu.com>
2021-07-08 13:16:28 +0800

    final

--- code1.cpp
+++ code1.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main()
 {
-    int n,k=0;      //n为要输入的数，k为n的二进制位数
+    char str[100];	//str用来储存步骤
+    int n,k=0,m=0;      //n为要输入的数，k为n的二进制位数
     int a[100]={0};     //数组a[100]用来储存n的逆二进制数
     cin>>n;
     while(n){
@@ -11,19 +12,22 @@ int main()
     }
     for(int i=0;i<k;i++){
         if(a[i]==0)
-            cout<<'^';
+            str[m++]='^';
         if(a[i]==1){
             if (a[i+1]==0&&i!=(k-1)){
                 a[i]=0;
-                cout<<'-'<<'^';
+                str[m++]='-';
+                str[m++]='^';
             }
             if(a[i+1]==1){
                 int j = i;
                 if(a[j]==1&&a[j+1]==1&&j==k-2){
-                    cout<<'-'<<'^';
+                    str[m++]='-';
+                    str[m++]='^';
                 }
                 else{
-                    cout<<'+'<<'^';
+                    str[m++]='+';
+                    str[m++]='^';
                     while(a[j]){
                         a[j] = 0;
                         j++;
@@ -33,6 +37,9 @@ int main()
             }
         }
     }
+    str[m]='\0';
+    cout<<m<<endl;
+    cout<<str;
     return 0;
 }
 
