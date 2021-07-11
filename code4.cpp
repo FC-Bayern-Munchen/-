6275aa143185eea386560db5d830840556dde506
wanglu_pro <wanglu_pro@zzu.com>
2021-07-11 13:29:10 +0800

    将文件名存入结构体，并将后缀名等分开

--- code4.cpp
+++ code4.cpp
@@ -1,38 +1,70 @@
+#include<bits/stdc++.h>
 #include<io.h>
 #include<direct.h>
 using namespace std;
 double getfilesize(char* filename);
 void getFiles( string, vector<string>& );
 void outsize(char * filename);
+void print();
+struct file{
+	char s1[50];//含路径的文件名
+	char s2[50];//文件名不含路径 
+	char s3[10];//文件名，不含路径和后缀名 
+	char s4[10];//扩展名 
+}f[30]; 
+int size;
+
 int main()
 {
-	int len;
+	int len1,len2,j;
 	char * filePath;
-	char s[30];
-	cin>>s;	
-		//字符串s为文件夹的路径 C:\Users\86175\Desktop\文件夹
-	len=strlen(s);
+	char s[50];
+	cin>>s;	  //字符串s为文件夹的路径 C:\Users\86175\Desktop\文件夹
+	len1=strlen(s);//len为文件夹路径的字符串长度 
 	filePath=s;
     vector<string> files;
 	getFiles(filePath,files);
 	char str[50];
-	char name[10][50];//储存文件全名含路径
-	char sname[10][15]; //储存文件名 
-	int size = files.size();
-		//文件个数   files[i].c_str()为文件夹内文件路径 
-	for(int i=0;i<size;i++){
-		strcpy(name[i],files[i].c_str());
-		int k=0;
-		for(int j=len+1;name[i][j]!='\0';j++)
-			sname[i][k++]=name[i][j];
-		sname[i][k]='\0';
-		cout<<sname[i]<<' '<<getfilesize(name[i])<<' ';
-		outsize(name[i]);
+	size = files.size();	//size为文件个数   files[i].c_str()为文件夹内文件路径 
+	for(int i=0;i<size;i++){ 
+		strcpy(f[i].s1,files[i].c_str());
+		len2=strlen(f[i].s1);
+		int k=0,m=0;
+		if(f[i].s1[len1+1]=='.'){
+			f[i].s4[0]='.';
+			f[i].s4[1]='\0';
+			for(j=len1+1;f[i].s1[j]!='\0';j++){
+				f[i].s2[k++]=f[i].s1[j];
+			} 
+			strcpy(f[i].s3,f[i].s2);
+		}
+		else{
+			for(j=len1+1;(f[i].s1[j]!='.')&&(f[i].s1[j]!='\0');j++){
+				f[i].s2[k]=f[i].s1[j];
+				f[i].s3[k]=f[i].s1[j];
+				k++;
+			}
+			if(j>=len2){
+				f[i].s4[0]='.';
+				f[i].s4[1]='\0';
+			}
+			else{
+				f[i].s2[k++]='.';
+				while(f[i].s1[j]!='\0'){
+					f[i].s4[m++]=f[i].s1[j++];
+					f[i].s2[k++]=f[i].s1[j];
+				}
+			}
+		}
+		/*
+		cout<<f[i].s2<<' '<<getfilesize(f[i].s1)<<' ';
+		outsize(f[i].s1);
 		cout<<endl;
+		*/
 	}
-	
-}
-void getFiles( string path, vector<string>& files )
+	print();
+ } 
+ void getFiles( string path, vector<string>& files )
 {
    long  hFile=0;
     struct _finddata_t fileinfo;
@@ -83,3 +115,14 @@ void outsize(char * filename)		//将文件的大小按照要求转换并打印
 		printf("G");
 	}
 }
+void print()
+{
+	for(int i=0;i<size;i++){
+		printf("%s\n",f[i].s1);
+		printf("%s\n",f[i].s2);
+		printf("%s\n",f[i].s3);
+		printf("%s\n",f[i].s4);
+		printf("\n");
+	}
+}
+
